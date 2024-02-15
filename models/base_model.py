d3919c917eda4d6a99d29f1c7ccd2977692057fb
TimmyAkinyemi123 <akinyemitimilehin044@gmail.com>
2024-02-15 09:32:24 -0800

    removed base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,73 +1,44 @@
 #!/usr/bin/python3
-"""Defines the BaseModel class."""
-import models
-from uuid import uuid4
-from datetime import datetime
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column
-from sqlalchemy import DateTime
-from sqlalchemy import String
+""" This script defines the BaseModel. """
 
-Base = declarative_base()
+import models
+import uuid
+from datetime import datetime
 
 
 class BaseModel:
-    """
-    Defines the BaseModel class.
-    Attributes:
-        id (sqlalchemy String): The BaseModel id.
-        created_at (sqlalchemy DateTime): The datetime at creation.
-        updated_at (sqlalchemy DateTime): The datetime of last update.
-    """
-
-    id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-
+    """ Defines all common attributes/methods for other classes"""
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel.
-        Args:
-            *args (any): Unused.
-            **kwargs (dict): Key/value pairs of attributes.
-        """
-        self.id = str(uuid4())
-        self.created_at = self.updated_at = datetime.utcnow()
+        """ Initializes the BaseModel attributes """
         if kwargs:
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__":
+                if key != '__class__':
+                    if key in ('created_at', 'updated_at'):
+                        value = datetime.strptime(
+                                value, '%Y-%m-%dT%H:%M:%S.%f')
                     setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+
+    def __str__(self):
+        """ String representation of objects """
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Update updated_at with the current datetime."""
-        self.updated_at = datetime.utcnow()
-        models.storage.new(self)
+        """ Updates the updated_at attribute to current time """
+        self.updated_at = datetime.now()
         models.storage.save()
 
-    def __str__(self):
-    """Return the print/str representation of the BaseModel instance."""
-    attributes = ", ".join(f"'{k}': '{v}'" for k, v in self.to_dict().items())
-    return f"[{type(self).__name__}] ({self.id}) {{{attributes}}}"
-
     def to_dict(self):
-        """Return a dictionary representation of the BaseModel instance.
-        Includes the key/value pair __class__ representing
-        the class name of the object.
-        """
-        my_dict = self.__dict__.copy()
-        my_dict["__class__"] = str(type(self).__name__)
-        my_dict["created_at"] = self.created_at.isoformat()
-        my_dict["updated_at"] = self.updated_at.isoformat()
-        my_dict.pop("_sa_instance_state", None)
-        return my_dict
-
-    def delete(self):
-        """Delete the current instance from storage."""
-        models.storage.delete(self)
-
-    def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
-        d = self.__dict__.copy()
-        d.pop("_sa_instance_state", None)
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, d)
+        """Returns a dictionary containing all
+        keys/values of __dict__ of the instance"""
+        data = self.__dict__.copy()
+        data['__class__'] = self.__class__.__name__
+        data['created_at'] = self.created_at.isoformat()
+        data['id'] = self.id
+        data['updated_at'] = self.updated_at.isoformat()
+        return data
