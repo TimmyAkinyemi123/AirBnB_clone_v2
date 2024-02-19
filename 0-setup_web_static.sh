686a8f82d24e636d594c457cf0561489a44c29c7
KishitheCreator <120695688+TimmyAkinyemi123@users.noreply.github.com>
2024-02-19 10:20:46 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,60 +1,27 @@
 #!/usr/bin/env bash
-# Sets up your web servers for the deployment of web_static.
+# script that sets up web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
-# Install Nginx Web Server
-sudo apt -y update
-sudo apt install -y nginx
-
-mkdir -p /data/
-mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
-touch /data/web_static/releases/test/index.html
-printf %s "
-<html>
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>
-" > /data/web_static/releases/test/index.html
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-ln -sf /data/web_static/releases/test /data/web_static/current
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
-touch default
-printf %s "server {
-        listen 80;
-        listen [::]:80;
-        add_header X-Served-By $HOSTNAME;
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
-        root /var/www/school;
-        index index.html;
-
-        location /redirect_me {
-            return 301 https://youtube.com;
-        }
-
-        location /hbnb_static/ {
-            alias /data/web_static/current/;
-            index index.html;
-        }
-
-        error_page 404 /404.html;
-        location = /404.html {
-            root /var/www/school/errors/;
-        }
-}
-" > default
-
-# Remove and replace existing configurations
-sudo rm -rf /etc/nginx/sites-available/default
-sudo rm -rf /etc/nginx/sites-enabled/default
-sudo mv default /etc/nginx/sites-available/
-sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
-
-# Restart nginx
 sudo service nginx restart
