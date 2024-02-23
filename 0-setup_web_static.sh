e849b486ebd224cad758b08507d3d1555826f920
KishitheCreator <120695688+TimmyAkinyemi123@users.noreply.github.com>
2024-02-23 21:43:42 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,45 +1,14 @@
 #!/usr/bin/env bash
-# Sets up web servers for deployment of web_static.
+# set up dummy html
 
-apt-get update
-apt-get install -y nginx
-
-mkdir -p /data/web_static/shared /data/web_static/releases/test
-printf %s "<html>
-	<head>
-	</head>
-	<body>
-		Holberton School
-	</body>
-</html>
-" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-chown -R ubuntu /data
-chgrp -R ubuntu /data
-
-printf %s "server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
-	add_header X-Served-By $HOSTNAME;
-	root /var/www/html;
-	index index.html index.htm;
-
-	location /hbnb_static {
-		alias /data/web_static/current;
-		index index.html index.htm;
-	}
-
-	location /redirect_me {
-		return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-	}
-
-	error_page 404 /404.html;
-	location /404 {
-		root /var/www/html;
-		internal;
-	}
-}" > /etc/nginx/sites-available/default
-
-service nginx restart
-exit 0
+server="\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
+file="/etc/nginx/sites-available/default"
+sudo apt-get update -y
+sudo apt-get install nginx -y
+sudo mkdir -p "/data/web_static/releases/test/"
+sudo mkdir "/data/web_static/shared/"
+echo "Holberton" > "/data/web_static/releases/test/index.html"
+rm -f "/data/web_static/current"; ln -s "/data/web_static/releases/test/" "/data/web_static/current"
+sudo chown -hR ubuntu:ubuntu "/data/"
+sudo sed -i "29i\ $server" "$file"
+sudo service nginx restart
