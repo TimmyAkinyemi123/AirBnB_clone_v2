bc511400f04c6590413de0c685e4a2ecfd404772
timmyakinyemi <akinyemitimilehin044@gmail.com>
2024-05-30 05:24:48 -0700

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,7 +4,7 @@ from fabric.api import *
 import os.path
 
 env.user = 'ubuntu'
-env.hosts = ["54.236.8.114", "100.25.167.153"]
+env.hosts = ["100.25.167.169", "100.25.196.96"]
 env.key_filename = "~/.ssh/school"
 
 
